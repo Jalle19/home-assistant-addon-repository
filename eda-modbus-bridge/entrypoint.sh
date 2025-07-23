5efdee431a227b6bcfd67861582738480a65871f
Sam Stenvall <sam.stenvall@nitor.com>
2025-07-23 14:28:10 +0300

    Remove legacy --unhandled-rejections=warn behavior
    This is unwanted, we want to fail hard and have the addon watchdog restart us, especially after https://github.com/Jalle19/eda-modbus-bridge/pull/151

--- eda-modbus-bridge/entrypoint.sh
+++ eda-modbus-bridge/entrypoint.sh
@@ -4,4 +4,4 @@ source config.sh
 
 get-config
 
-node --unhandled-rejections=warn /app/eda-modbus-bridge/dist/eda-modbus-bridge.js ${CMD_OPTIONS}
+node /app/eda-modbus-bridge/dist/eda-modbus-bridge.js ${CMD_OPTIONS}
